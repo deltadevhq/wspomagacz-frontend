f4e68d415e674fe6a8801dc9e154740f202400da
Remigiusz Nowicki <remigiusz.nowicki11@gmail.com>
2024-09-13 02:26:11 +0200

    update home component

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,42 @@
+import { Component, effect, inject, input } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
+import { NgForOf } from '@angular/common';
+import { HomeCalendarComponent } from './ui/home-calendar/home-calendar.component';
+import { AuthService, AuthUser } from '../shared/data-access/auth.service';
+import { Router } from '@angular/router';
+import { LevelProgressComponent } from './ui/level-progress/level-progress.component';
+import { AvatarLevelProgressComponent } from './ui/avatar-level-progress/avatar-level-progress.component';
+
+@Component({
+    standalone: true,
+    selector: 'app-home-page',
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.component.scss'],
+    imports: [
+        IonicModule,
+        NgForOf,
+        HomeCalendarComponent,
+        LevelProgressComponent,
+        AvatarLevelProgressComponent,
+    ],
+})
+export class HomeComponent {
+    public authService = inject(AuthService);
+    private router = inject(Router);
+
+    constructor() {
+        effect(() => {
+            if (!this.authService.user()) {
+                this.router.navigate(['welcome']);
+            }
+        });
+    }
+
+    //protected readonly levelProgressDegree: number =
+    //    this.calculateLevelProgressDegree(this.levelProgress);
+
+    //protected calculateLevelProgressDegree(levelProgress: number): number {
+    // TODO: Get level for current exp from API, for now just assume 100 exp per level
+    //return 360 * levelProgress;
+    //}
+}
