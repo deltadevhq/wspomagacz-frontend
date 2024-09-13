007ee0bc3b3d1650f7f7ec343bab928533f10e43
Remigiusz Nowicki <remigiusz.nowicki11@gmail.com>
2024-09-13 02:17:59 +0200

    create workouts component

---
+++ src/app/workouts/workouts.component.ts
@@ -0,0 +1,9 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    standalone: true,
+    selector: 'app-workouts',
+    templateUrl: './workouts.component.html',
+    styleUrls: ['./workouts.component.scss'],
+})
+export class WorkoutsComponent {}
