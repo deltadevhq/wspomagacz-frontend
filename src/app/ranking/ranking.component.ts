3f7a9ef020b977b16bae900fda32831bcbd195c6
Remigiusz Nowicki <remigiusz.nowicki11@gmail.com>
2024-09-13 02:20:26 +0200

    create ranking component

---
+++ src/app/ranking/ranking.component.ts
@@ -0,0 +1,9 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    standalone: true,
+    selector: 'app-ranking',
+    templateUrl: './ranking.component.html',
+    styleUrls: ['./ranking.component.scss'],
+})
+export class RankingComponent {}
