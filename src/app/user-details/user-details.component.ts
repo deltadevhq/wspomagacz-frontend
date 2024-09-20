c8f834535adf1d237c45ec0458562e6440243512
Remigiusz Nowicki <remigiusz.nowicki11@gmail.com>
2024-09-20 14:10:43 +0200

    Create user-details page

---
+++ src/app/user-details/user-details.component.ts
@@ -0,0 +1,9 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    standalone: true,
+    selector: 'app-user-details',
+    templateUrl: './user-details.component.html',
+    styleUrls: ['./user-details.component.scss'],
+})
+export class UserDetailsComponent {}
