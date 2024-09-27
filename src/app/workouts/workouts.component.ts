44aaddad6afd27864d54f084d5bce43377419e28
Remigiusz Nowicki <remigiusz.nowicki11@gmail.com>
2024-09-27 18:36:01 +0200

    Add list of predefined workouts and use new Workout Calendar flow

--- src/app/workouts/workouts.component.ts
+++ src/app/workouts/workouts.component.ts
@@ -4,8 +4,10 @@ import { AvatarLevelProgressComponent } from '../home/ui/avatar-level-progress/a
 import { HomeCalendarComponent } from '../home/ui/home-calendar/home-calendar.component';
 import { IonicModule } from '@ionic/angular';
 import { LevelProgressComponent } from '../home/ui/level-progress/level-progress.component';
-import { NgForOf } from '@angular/common';
+import { AsyncPipe, NgForOf, NgIf } from '@angular/common';
 import { AuthService } from '../shared/data-access/auth.service';
+import { WorkoutInfoComponent } from '../workout-info/workout-info.component';
+import { Workout } from '../shared/models/Workout';
 
 @Component({
     standalone: true,
@@ -19,8 +21,22 @@ import { AuthService } from '../shared/data-access/auth.service';
         IonicModule,
         LevelProgressComponent,
         NgForOf,
+        AsyncPipe,
+        NgIf,
+        WorkoutInfoComponent,
     ],
 })
 export class WorkoutsComponent {
     authService = inject(AuthService);
+
+    selectedWorkout?: Workout;
+    selectedWorkoutDuration?: number;
+
+    onSelectedWorkoutChange(workout: Workout) {
+        this.selectedWorkout = workout;
+    }
+
+    onSelectedWorkoutDurationChange(duration?: number) {
+        this.selectedWorkoutDuration = duration;
+    }
 }
