07982158c8e1eadcbd727aa6ccb81bcf9f3fab71
Remigiusz Nowicki <remigiusz.nowicki11@gmail.com>
2024-10-27 07:58:52 +0100

    Refactor routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,79 +1,45 @@
 import { Routes } from '@angular/router';
+import { TabsComponent } from './tabs/tabs.component';
 import { isAuthenticatedGuard } from './shared/guards/auth.guard';
 
 export const routes: Routes = [
-    {
-        path: 'welcome',
-        pathMatch: 'full',
-        loadComponent: () =>
-            import('./welcome/welcome.component').then(
-                (m) => m.WelcomeComponent,
-            ),
-    },
-    {
-        path: 'login',
-        loadComponent: () =>
-            import('./login/login.component').then((m) => m.LoginComponent),
-    },
-    {
-        path: 'register',
-        loadComponent: () =>
-            import('./register/register.component').then(
-                (m) => m.RegisterComponent,
-            ),
-    },
-    {
-        path: '',
-        loadComponent: () =>
-            import('./tabs/tabs.component').then((m) => m.TabsComponent),
-        canActivate: [isAuthenticatedGuard()], // Ensure user IS authenticated
-        children: [
-            {
-                path: 'home',
-                loadComponent: () =>
-                    import('./home/home.component').then(
-                        (m) => m.HomeComponent,
-                    ),
-            },
-            {
-                path: 'workouts',
-                loadComponent: () =>
-                    import('./workouts/workouts.component').then(
-                        (m) => m.WorkoutsComponent,
-                    ),
-                children: [
-                    {
-                        path: ':id',
-                        loadComponent: () =>
-                            import(
-                                './workout-details/workout-details.component'
-                            ).then((m) => m.WorkoutDetailsComponent),
-                    },
-                ],
-            },
-            {
-                path: 'ranking',
-                loadComponent: () =>
-                    import('./ranking/ranking.component').then(
-                        (m) => m.RankingComponent,
-                    ),
-            },
-            {
-                path: 'users',
-                children: [
-                    {
-                        path: ':id',
-                        loadComponent: () =>
-                            import(
-                                './user-details/user-details.component'
-                            ).then((m) => m.UserDetailsComponent),
-                    },
-                ],
-            },
-            { path: '', redirectTo: 'home', pathMatch: 'full' }, // Default to home
-            { path: '**', redirectTo: 'home' }, // Redirect any unknown route to home
-        ],
-    },
-    { path: '', redirectTo: 'welcome', pathMatch: 'full' }, // Redirect any unknown route to welcome page
-    { path: '**', redirectTo: 'welcome' }, // Redirect any unknown route to welcome page
+  { path: '', redirectTo: '/welcome', pathMatch: 'full' },
+  {
+    path: 'welcome',
+    loadComponent: () => import('./welcome/welcome.page').then(m => m.WelcomePage),
+  },
+  {
+    path: 'login',
+    loadComponent: () => import('./login/login.page').then(m => m.LoginPage),
+  },
+  {
+    path: 'register',
+    loadComponent: () => import('./register/register.page').then(m => m.RegisterPage),
+  },
+  {
+    path: 'tabs',
+    component: TabsComponent,
+    canActivate: [isAuthenticatedGuard()],
+    children: [
+      {
+        path: 'home',
+        loadComponent: () => import('./home/home.page').then(m => m.HomePage),
+      },
+      {
+        path: 'workouts',
+        loadComponent: () => import('./workouts/workouts.page').then(m => m.WorkoutsPage),
+      },
+      {
+        path: 'profile',
+        loadComponent: () => import('./profile/profile.page').then(m => m.ProfilePage),
+      },
+      { path: '', redirectTo: 'tabs/home', pathMatch: 'full' },
+      { path: '**', redirectTo: 'tabs/home' },
+    ],
+  },
+  { path: '**', redirectTo: '/welcome' },
+  {
+    path: 'ranking',
+    loadComponent: () => import('./ranking/ranking.page').then(m => m.RankingPage),
+  },
 ];
