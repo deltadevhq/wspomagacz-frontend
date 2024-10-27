ef0828abdec5905bcdfbd79ccb72efe509c0eeed
Remigiusz Nowicki <remigiusz.nowicki11@gmail.com>
2024-10-27 08:07:48 +0100

    Refactor

--- src/app/register/register-form/register-form.component.ts
+++ src/app/register/register-form/register-form.component.ts
@@ -2,24 +2,24 @@ import { Component, inject, input, output } from '@angular/core';
 import { CreateUserCredentials } from '../../shared/data-access/auth.service';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { RegisterStatus } from '../register.service';
-import { IonicModule } from '@ionic/angular';
+import { IonButton, IonInput, IonInputPasswordToggle, IonToast } from '@ionic/angular/standalone';
 
 @Component({
-    standalone: true,
-    selector: 'app-register-form',
-    templateUrl: './register-form.component.html',
-    styleUrls: ['./register-form.component.scss'],
-    imports: [IonicModule, ReactiveFormsModule],
+  standalone: true,
+  selector: 'app-register-form',
+  templateUrl: './register-form.component.html',
+  styleUrls: ['./register-form.component.scss'],
+  imports: [ReactiveFormsModule, IonInput, IonInputPasswordToggle, IonToast, IonButton],
 })
 export class RegisterFormComponent {
-    registerStatus = input.required<RegisterStatus>();
-    createUser = output<CreateUserCredentials>();
+  registerStatus = input.required<RegisterStatus>();
+  createUser = output<CreateUserCredentials>();
 
-    private fb = inject(FormBuilder);
+  private fb = inject(FormBuilder);
 
-    registerForm = this.fb.nonNullable.group({
-        username: ['', [Validators.required, Validators.minLength(3)]],
-        email: ['', [Validators.required, Validators.email]],
-        password: ['', [Validators.required, Validators.minLength(8)]],
-    });
+  registerForm = this.fb.nonNullable.group({
+    username: ['', [Validators.required, Validators.minLength(3)], Validators.maxLength(30), Validators.pattern(/^[a-zA-Z0-9]*$/)],
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(8), Validators.maxLength(30)]],
+  });
 }
