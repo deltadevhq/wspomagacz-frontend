fa3332b6f75b88efac49cc8f4783999052711ca0
Remigiusz Nowicki <remigiusz.nowicki11@gmail.com>
2024-10-27 08:00:29 +0100

    Replace Home component with page

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,52 @@
+import { Component, effect, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import {
+  IonCard,
+  IonCardContent,
+  IonCardHeader,
+  IonCardTitle,
+  IonCol,
+  IonContent,
+  IonGrid,
+  IonHeader,
+  IonIcon,
+  IonRow,
+  IonSpinner,
+  IonText,
+  IonTitle,
+  IonToolbar,
+} from '@ionic/angular/standalone';
+import { CalendarComponent } from '../shared/ui/calendar/calendar.component';
+import { AuthService } from '../shared/data-access/auth.service';
+import { Router } from '@angular/router';
+import { NotificationsComponent } from '../shared/ui/notifications/notifications.component';
+import { WorkoutSummaryComponent } from '../shared/ui/workout-summary/workout-summary.component';
+import { Workout } from '../shared/models/Workout';
+import { WorkoutsCalendarComponent } from '../shared/ui/calendar/workouts-calendar/workouts-calendar.component';
+import { BehaviorSubject } from 'rxjs';
+import { AvatarLevelProgressComponent } from '../shared/ui/avatar-level-progress/avatar-level-progress.component';
+import { LevelProgressComponent } from '../shared/ui/level-progress/level-progress.component';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.page.html',
+  styleUrls: ['./home.page.scss'],
+  standalone: true,
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, AvatarLevelProgressComponent, NotificationsComponent, IonText, IonGrid, IonRow, IonCol, LevelProgressComponent, CalendarComponent, CalendarComponent, WorkoutsCalendarComponent, WorkoutSummaryComponent, IonCard, IonCardContent, IonCardHeader, IonCardTitle, IonIcon, IonSpinner, AvatarLevelProgressComponent, LevelProgressComponent],
+})
+export class HomePage {
+  public authService = inject(AuthService);
+  private router = inject(Router);
+
+  selectedWorkout$ = new BehaviorSubject<Workout | undefined>(undefined);
+  selectedDate$ = new BehaviorSubject<Date>(new Date());
+
+  constructor() {
+    effect(() => {
+      if (!this.authService.user()) {
+        this.router.navigate(['/welcome']);
+      }
+    });
+  }
+}
