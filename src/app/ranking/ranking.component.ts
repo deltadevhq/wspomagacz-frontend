da68a6db60493b0da17366e48ba200905debb5eb
Remigiusz Nowicki <remigiusz.nowicki11@gmail.com>
2024-10-27 08:11:51 +0100

    Replace Ranking component with page

--- src/app/ranking/ranking.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component, OnInit } from '@angular/core';
-
-@Component({
-    standalone: true,
-    selector: 'app-ranking',
-    templateUrl: './ranking.component.html',
-    styleUrls: ['./ranking.component.scss'],
-})
-export class RankingComponent {}
